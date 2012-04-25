5b8521f8fd57cedf41a355466b8caade3b4b7447
thefrosty <austin@thefrosty.com>
2012-04-25 10:19:25 -0700

    Version 1.1

---
+++ README.md
@@ -0,0 +1,40 @@
+SEO Media File Renamer
+======================
+Contributors: austyfrosty
+Donate link: [PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7431290)
+Tags: seo, media, rename, images
+Requires at least: 3.2
+Tested up to: 3.4
+Stable tag: 1.1
+
+Optimize your media images by renaming the files. It adds a field in the media editor form where you can enter a new file name&sup1;. After renaming it will update the database with the new file name and location. Once complete the plugin will remove all old images included all sizes, ie. (large, medium, thumbnails and all custom image sizes). Once all old images are removed new custom sizes are generated then the plugin looks though all posts and updates any links with the old file URL to the new URL. 
+
+Installation
+============
+
+1. Upload `seo-media-file-renamer` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Change any file name through the 'Media' editor page.
+4. This plugin auto updates through GitHub!
+
+Screenshots
+===========
+
+1. Example of new file name field on media edit form
+
+Changelog
+=========
+
+### 1.1
+* Added translation files.
+* Added GitHub updater script.
+
+### 1.0
+* Initial released fork from `Rename Media Files`.
+
+Current Version
+===============
+
+The line below is used for the updater API, please leave it untouched unless bumping the version up :)
+
+~Current Version:1.1~
--- seo-media-file-renamer.php
+++ seo-media-file-renamer.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name: SEO Media File Renamer
- * Plugin URI:  http://austinpassy.com/wordpress-plugins/seo-media-file-renamer/
- * Description: Rename media files for SEO purposes.
- * Author:      Austin Passy
- * Author URI:  http://austinpassy.com/
- * Version:     1.0
- * License:     GPL
- * Text Domain: seo-media-file-renamer
- * Domain Path: /languages/
+ * Plugin Name:	SEO Media File Renamer
+ * Plugin URI:	http://austinpassy.com/wordpress-plugins/seo-media-file-renamer/
+ * Description:	Rename media files for SEO purposes.
+ * Author:		Austin Passy
+ * Author URI:	http://austinpassy.com/
+ * Version:     1.1
+ * License:		GPL
+ * Text Domain:	seo-media-file-renamer
+ * Domain Path:	/languages/
  */
 
 /**
@@ -18,7 +18,12 @@
  * 	Media Tags
  * 	WP Smush.it
  * 	AJAX Thumbnail Rebuild
- *  Rename Media Files
+ *	Rename Media
+ *	Rename Media Files
+ *
+ * Forked from:
+ *
+ *	Rename Media Files
  */
 
 /* Exit if accessed directly */
@@ -34,12 +39,28 @@ add_action( 'init', 'seo_media_file_renamer_init' );
  */
 function seo_media_file_renamer_init() {
 
-	/* Add filters to load & save media edit screen for field */
+	/* Add filters to load and save media edit screen for field */
 	add_filter( 'attachment_fields_to_edit', 'seo_media_file_renamer_fields_to_edit', 11, 2 );
 	add_filter( 'attachment_fields_to_save', 'seo_media_file_renamer_attachment_fields_to_save', 11, 2 );
 
 	/* Show donate link in plugin's links row */
 	add_filter( 'plugin_action_links', 'seo_media_file_renamer_filter_plugin_actions', 10, 2 );
+
+	if ( is_admin() ) {
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ), // this is the slug of your plugin
+			'proper_folder_name' => 'seo-media-file-renamer', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/thefrosty/seo-media-file-renamer', // the github API url of your github repo
+			'raw_url' => 'https://raw.github.com/thefrosty/master/seo-media-file-renamer', // the github raw url of your github repo
+			'github_url' => 'https://github.com/thefrosty/seo-media-file-renamer', // the github url of your github repo
+			'zip_url' => 'https://github.com/thefrosty/seo-media-file-renamer/zipball/master', // the zip url of the github repo
+			'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.3', // which version of WordPress does your plugin require?
+			'tested' => '3.4', // which version of WordPress is your plugin tested up to?
+		);
+		if ( !class_exists( 'WPGitHubUpdater' ) ) require_once( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'updater.php' );
+		new WPGitHubUpdater( $config );
+	}
 }
 
 /**
@@ -100,15 +121,15 @@ function seo_media_file_renamer_fields_to_edit( $form_fields, $post ) {
 	$old_filename = get_post_meta( $post->ID, '_original_filename', true );
 
 	/* Setup a new field */
-    $form_fields['seo_media_file_renamer_input'] = array(
+	$form_fields['seo_media_file_renamer_input'] = array(
        	'label' => __( 'New file name:', 'seo-media-file-renamer' ),
    		'value' => '',
-		'helps' => sprintf( __( 'Enter a new file name in the field above. (current filename is <strong>%1$s</strong>)', 'seo-media-file-renamer'), $orig_filename )
+		'helps' => sprintf( __( 'Enter a new file name in the field above. (current filename is %1$s)', 'seo-media-file-renamer'), '<strong>' . $orig_filename . '</strong>' )
 	);
 	$form_fields['original_media_file_name'] = array(
        	'label' => __( 'Original file name:', 'seo-media-file-renamer' ),
 		'input' => 'html',
-		'html' => "<input type='text' class='text' readonly='readonly' name='attachments[$post->ID][original_media_file_name]' value='" . esc_attr($old_filename) . "' /><br />",
+		'html' => "<input type='text' class='text' readonly='readonly' name='attachments[$post->ID][original_media_file_name]' value='" . esc_attr( $old_filename ) . "' /><br />",
    		'value' => $old_filename
 	); //*/
 	//print '<pre>'; print_r( $form_fields ); print '</pre>';
@@ -127,8 +148,9 @@ function seo_media_file_renamer_fields_to_edit( $form_fields, $post ) {
  */
 function seo_media_file_renamer_attachment_fields_to_save( $post, $attachment ) {
 
-	/* Only proceed if new filename is submitted */
+	/* Only proceed if a new filename is submitted */
 	if ( $attachment['seo_media_file_renamer_input'] ) {
+		
 		/* Get original filename */
 		$orig_file = get_attached_file( $post['ID'] );
 		$orig_filename = basename( $orig_file );
@@ -159,18 +181,18 @@ function seo_media_file_renamer_attachment_fields_to_save( $post, $attachment ) 
 		$new_filename = strtolower( $new_filename );
 		$new_file = $orig_dir_path . "/" . $new_filename . $extension; // DIRECTORY_SEPARATOR == "/"
 		
-		// Check if new file exists
+		/* Check if new file exists */
 		if ( file_exists( $new_file ) === false ) {
 			$original_filename = get_post_meta( $post['ID'], '_original_filename', true );
 			if ( empty( $original_filename ) )
 				add_post_meta( $post['ID'], '_original_filename', $orig_filename );
 		}
 		
-		// Ensure attachment meta changes
+		/* Ensure attachment meta changes */
 		$post['post_name'] = ( !empty( $post['post_title'] ) ) ? sanitize_title( $post['post_title'] ) : sanitize_title( basename( $new_filename ) );
 		$post['guid'] = str_replace( $orig_filename, $new_filename, $post['guid'] );
 
-		// Save
+		/* Save */
 		wp_update_post( $post );
 
 		/* Make new file with desired name */
@@ -179,7 +201,7 @@ function seo_media_file_renamer_attachment_fields_to_save( $post, $attachment ) 
 		/* Update file location in database */
 		update_attached_file( $post['ID'], $new_file );
 		
-		/* Remove the old meta files */
+		/* Remove the old intermediate media files */
 		$meta = wp_get_attachment_metadata( $post['ID'] );
 		foreach ( (array)$meta['sizes'] as $size => $meta_size ) {
 			$old_file = dirname( $orig_file ) . "/" . $meta['sizes'][$size]['file'];
@@ -212,9 +234,9 @@ function seo_media_file_renamer_attachment_fields_to_save( $post, $attachment ) 
 			$wpdb->query( $wpdb->prepare( "UPDATE $wpdb->posts SET post_content = REPLACE(post_content, '$orig_attachment_url', '$new_attachment_url');" ) );
 		}
 		
-	}
+	} // end_if
 
-    return $post;
+	return $post;
 }
 
 ?>
